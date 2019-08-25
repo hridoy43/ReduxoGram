b7dc416226c6b229ad1cb88c163ebcba216eb5b7
Wahiduzzaman Hridoy <hridoy43@gmail.com>
2019-08-25 23:18:07 +0600

    new directory

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+    render() {
+        return (
+            <div className="photo-grid">
+                I'm the Single.
+            </div>
+        );
+    }
+
+}
+export default Single;
---
+++ client/reduxstagram.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+//import css
+import css from './styles/style.styl'
+
+render(<Main />, document.getElementById('root'));
