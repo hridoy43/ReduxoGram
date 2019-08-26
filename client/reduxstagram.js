1b125a8664e7d7c5fa61ba0974cfa096ad197758
hridoy43 <hridoy43@gmail.com>
2019-08-26 09:28:29 +0600

    router added

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
 //import css
 import css from './styles/style.styl'
 
-render(<Main><p>Hello!</p></Main>, document.getElementById('root'));
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
