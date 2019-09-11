e7243f8dff386d15a45c2fac6d7217581af5bc43
hridoy43 <hridoy43@gmail.com>
2019-09-11 16:51:30 +0600

    provider added

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import post from './posts';
+import posts from './posts';
 import comments from './comments';
 
-const rootReducer = combineReducers({ post, comments, router: routerReducer });
+const rootReducer = combineReducers({ posts, comments, routing: routerReducer });
 
 export default rootReducer;
