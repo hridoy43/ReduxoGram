51042cfa208b1b2d939f39757ed808543a96f079
Wahiduzzaman Hridoy <hridoy43@gmail.com>
2019-09-15 21:38:39 +0600

    single-photo component added

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
+//import comment
 class Single extends Component {
     render() {
+        const i = this.props.posts.findIndex((item) => item.code === this.props.params.postId)
+        const post = this.props.posts[i];
+        console.log(post);
+
         return (
-            <div className="photo-grid">
-                I'm the Single.
+            <div className="single-photo">
+                <Photo i={i} post={post} {...this.props} />
+                <Comments />
+
             </div>
         );
     }
